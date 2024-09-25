2dc5849a3a1313cc8b429d8454dbcfafc32f0468
L20660046 <L20660046@matehuala.tecnm.mx>
2024-09-25 17:43:36 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Esperamos a que el DOM se cargue completamente
+document.addEventListener("DOMContentLoaded", function() {
+    // Accedemos al elemento con id "soylend" y cambiamos su contenido
+    document.getElementById("soylend").textContent = "!No me gusta la programación¡";
+});
+
